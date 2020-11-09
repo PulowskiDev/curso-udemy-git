119df1c9b318c42a45dd042f3cee60738c1db8f4
Alberto Blasco <albertomuni_61@hotmail.com>
2020-11-09 20:05:16 +0100

    Modificacion del js para incluir mensaje

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('Curso de git');
+const container = document.getElementById('container');
+
+const div = document.createElement('div');
+const span = document.createElement('span');
+span.innerText = 'Esto es un ejemplo';
+
+container.appendChild(div.appendChild(span));
